ccfa5f93de37bf12776b2252aad2246c47870c1b
ChristianBaciu <christian3110@outlook.it>
2024-03-05 12:47:16 +0100

    querty con GROUP BY 1/2

--- index.php
+++ index.php
@@ -25,18 +25,16 @@
     SELECT *
     FROM `exams`
     WHERE `date` = '2020-06-20'
-    <!-- AND `hour` >=14; -->
+    AND HOUR (`hour`) >=14;
 
 6)Selezionare tutti i corsi di laurea magistrale (38)
     SELECT *
     FROM `degrees`
     WHERE `level` = 'magistrale';
 
-
 7)Da quanti dipartimenti è composta l'università? (12)
     SELECT *
     FROM `departments`;
-
 
 8)Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
     SELECT *
@@ -49,8 +47,16 @@
 
 1)Contare quanti iscritti ci sono stati ogni anno
 
+
 2)Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
+
 3)Calcolare la media dei voti di ogni appello d'esame
+    SELECT `exam_id`, AVG(`vote`)
+    FROM `exam_student`
+    GROUP BY `exam_id`
 
 4)Contare quanti corsi di laurea ci sono per ogni dipartimento
+    SELECT `department_id`, COUNT(*)
+    FROM `degrees`
+    GROUP BY `department_id`;
