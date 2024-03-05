45592dbd95553057ddc1ba60a4e70f61445eea07
ChristianBaciu <christian3110@outlook.it>
2024-03-05 23:57:54 +0100

    es.5 QUERY CON JOIN

--- index.php
+++ index.php
@@ -4,15 +4,18 @@
     FROM `students`
     WHERE YEAR(`date_of_birth`) = 1990;
 
+
 2) Selezionare tutti i corsi che valgono più di 10 crediti (479)
     SELECT *
     FROM `courses`
     WHERE `cfu` >10;
 
+
 3) Selezionare tutti gli studenti che hanno più di 30 anni
     SELECT *
     FROM `students`
     WHERE TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) > 30;
+
 
 4) Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
     SELECT *
@@ -20,20 +23,24 @@
     WHERE `period` = 'I semestre'
     AND `year` = 1;
 
+
 5) Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
     SELECT *
     FROM `exams`
     WHERE `date` = '2020-06-20'
     AND HOUR (`hour`) >=14;
 
+
 6) Selezionare tutti i corsi di laurea magistrale (38)
     SELECT *
     FROM `degrees`
     WHERE `level` = 'magistrale';
 
+
 7) Da quanti dipartimenti è composta l'università? (12)
     SELECT *
     FROM `departments`;
+
 
 8) Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
     SELECT *
@@ -43,21 +50,22 @@
 
 <!-- querty con GROUP BY -->
 1) Contare quanti iscritti ci sono stati ogni anno
-    <!-- con AS assegno un nuovo nome -->
     SELECT YEAR(enrolment_date) AS `anno`, COUNT(*) AS `studenti`
     FROM `students`
     GROUP BY `anno`;
 
-2) Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
+2) Contare gli insegnanti che hanno l'ufficio nello stesso edificio
     SELECT COUNT(id), `office_address`
     FROM `teachers`
     GROUP BY `office_address`;
+
 
 3) Calcolare la media dei voti di ogni appello d'esame
     SELECT `exam_id`, AVG(`vote`)
     FROM `exam_student`
     GROUP BY `exam_id`
+
 
 4) Contare quanti corsi di laurea ci sono per ogni dipartimento
     SELECT `department_id`, COUNT(*)
@@ -67,12 +75,12 @@
 
 <!-- EX - QUERY CON JOIN -->
 1) Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-    <!-- con select sono preso solo le colonne che mi interessavano, ovvero il nome e cognome degli studenti e il corso -->
     SELECT `students`.`name` , `students`.`surname` , `degrees`.`name` , `degrees`.`website`
     FROM `students`
     JOIN `degrees`
     ON `students`.`degree_id` = `degrees`.`id`
     WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
 
 2) Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
     SELECT *
@@ -81,6 +89,7 @@
     ON `degrees`.`id` = `departments`.`id`
     WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
+
 3) Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
     SELECT *
     FROM `courses`
@@ -88,29 +97,58 @@
     ON `courses`.`id` = `teachers`.`id`
     WHERE `teachers`.`name` = 'Fulvio' OR  `teachers`.`name` = 'Amato'
 
+
 4) Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui
-sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e
-nome
-
-
+sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+    SELECT `students`.`name` , `students`.`surname` , `degrees`.`name` AS `Laurea` , `departments`.`name` AS `Dipartimento`
+    FROM `students`
+    JOIN `degrees`
+    ON `students`.`degree_id` = `degrees`.`id`
+    JOIN `departments`
+    ON `departments`.`id` = `degrees`.`id`
+    ORDER BY `students`.`name` , `students`.`surname` ASC;
 
 
 5) Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-    SELECT *
-    FROM `course_teacher`
-    JOIN `teachers`
-    ON
-
-
-
-
-
+    SELECT `teachers`.`name` , `teachers`.`surname` , `degrees`.`name` AS `Corsi di Laurea`
+    FROM `teachers`
+    JOIN `courses`
+    ON `teachers`.`id` = `courses`.`id`
+    JOIN `degrees`
+    ON `degrees`.`id` = `courses`.`degree_id`
+ 
 
 
 6) Selezionare tutti i docenti che insegnano nel Dipartimento di
 Matematica (54)
 
 
+
+
 7) BONUS: Selezionare per ogni studente il numero di tentativi sostenuti
 per ogni esame, stampando anche il voto massimo. Successivamente,
 filtrare i tentativi con voto minimo 18.
+
+
+
+
+<!-- seleziona tutto -->
+SELECT *
+<!-- da -->
+FROM `degrees`
+<!-- aggiungere courses, ha un legame con degrees -->
+JOIN `courses`
+<!-- prendiamo le info -->
+ON `degrees`.`id` = `courses`.`degree_id`<!-- dopo aver terminato la loro relazione -->
+<!-- ricominciamo con un nuovo JOIN -->
+JOIN `exams`
+<!-- prendiamo le info -->
+ON `exams`.`course_id` = `courses`.`id`
+
+<!-- con questo abbiamo ottenuto id della prima tab, id della seconda tabella e id della terza tabella -->
+
+WHERE `degrees`,`name` = 'Corso di Laurea Magistrale in Fisica'
+
+
+
+
